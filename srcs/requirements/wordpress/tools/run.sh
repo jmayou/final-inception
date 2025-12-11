597f64aa66fbd37fee260f72e5a68809c82dde79
jmayou <mayoujamila01@gmail.com>
2025-12-11 19:11:48 +0100

    niiiiiiice

--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -10,12 +10,12 @@ until wait_for_db; do
     echo "MariaDB not ready yet... waiting"
     sleep 1
 done
-echo "MariaDB is up!"
 
 if [ ! -f /var/www/html/wp-config.php ]; then
     wget https://wordpress.org/latest.tar.gz
     tar -xzf latest.tar.gz --strip-components=1
     rm latest.tar.gz
+
     sed -i "s/database_name_here/$MYSQL_DATABASE/" wp-config-sample.php
     sed -i "s/username_here/$MYSQL_USER/" wp-config-sample.php
     sed -i "s/password_here/$MYSQL_PASSWORD/" wp-config-sample.php
@@ -24,21 +24,20 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 fi
 
 if ! wp core is-installed --allow-root 2>/dev/null; then
-    echo "Installing WordPress..."
     wp core install \
-        --allow-root
         --url="${SITE_URL}" \
         --title="${SITE_TITLE}" \
         --admin_user="${WP_ADMIN_USER}" \
         --admin_password="${WP_ADMIN_PASSWORD}" \
         --admin_email="${WP_ADMIN_EMAIL}" \
-    echo "Creating normal user..."
+        --allow-root
     wp user create "${WP_USER}" "${WP_USER_EMAIL}" \
-        --allow-root 
-        --role=author \ 
+        --role=author \
         --user_pass="${WP_USER_PASSWORD}" \
+        --allow-root
 else
     echo "WordPress if installed"
 fi
+
 sed -i "s/^listen = .*/listen = 9000/" /etc/php/8.2/fpm/pool.d/www.conf
 exec php-fpm8.2 -F
